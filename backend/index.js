b338bed920631a3e49d2c5e7a4adf553d57c199f
Devinda Dilshan <dilshandevinda88@gmail.com>
2024-11-18 14:26:55 +0530

    Backend

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,11 @@ const app = express()
 const port = 3001
 
 const cors = require('cors')
-//const router = require('./Routes/router')
+const router = require('./Routes/router')
 
 app.use(cors());
 app.use(express.json());
-//app.use(router);
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
