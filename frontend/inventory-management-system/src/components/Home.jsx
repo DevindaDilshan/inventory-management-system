1115de95dddc2526b9e46e711185f86f004e8f2f
Devinda Dilshan <dilshandevinda88@gmail.com>
2024-11-29 21:55:44 +0530

    Refactor code

--- frontend/inventory-management-system/src/components/Home.jsx
+++ frontend/inventory-management-system/src/components/Home.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className=' p-5 d-flex justify-content-center'>
-        <h2>Welcome to Inventory Management System</h2>
+
+    <div className=' p-5 text-center justify-content-center container'>
+      <div classname="row"><h2>Welcome to Inventory Management System</h2></div>
+      <div classname="row"><p></p></div>
+      <div classname="row "><img height="300rem" src="/main-banner.jpg"></img></div>
     </div>
+
   )
 }
--- frontend/inventory-management-system/src/components/Navbar.jsx
+++ frontend/inventory-management-system/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar(props) {
   return (
     <div>
-      <navBar className="navbar navbar-expand-lg bg-danger">
+      <navBar className="navbar navbar-expand-lg bg-success">
         <div className="container-fluid">
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
